08ebad67f19c2bd6490c99e332dcf5386f6069e2
Eoin <eoin.t.moran00@gmail.com>
2020-06-07 00:24:28 +0100

    Modified client movement code to work with Mirror Networking

--- Movement.cs
+++ Movement.cs
@@ -1,6 +1,7 @@
-﻿using UnityEngine;
+﻿using Mirror;
+using UnityEngine;
 
-public class Movement : MonoBehaviour
+public class Movement : NetworkBehaviour
 {
     public float speed = 24.0f;
     public float jumpSpeed = 8.0f;
@@ -9,40 +10,50 @@ public class Movement : MonoBehaviour
     public float mouseSensitivity = 0.2f;
 
     private CharacterController controller;
-    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 moveDirection = Vector3 .zero;
     private Transform cameraTransform;
 
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
         cameraTransform = transform.GetChild(0);
+        if (isLocalPlayer)
+        {
+	        gameObject.transform.GetChild(0).gameObject.SetActive(true); // Enable the camera on the correct player
+        }
     }
-    
+
     void Update()
     {
-        transform.Rotate(new Vector3(0,  Input.GetAxis("Camera X"), 0) * mouseSensitivity);
-        cameraTransform.Rotate(new Vector3(Input.GetAxis("Camera Y"),  0, 0) * mouseSensitivity);
-        
-        
-        if (controller.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection); // Deal with rotation
-            moveDirection *= speed;
+	    if (!isLocalPlayer)
+	    {
+		    return;
+	    }
 
-            if (Input.GetButton("Sprint"))
-            {
-                moveDirection *= sprintMultiplier;
-            }
 
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
-        
-        moveDirection.y -= gravity * Time.deltaTime; // Gravity
-        
-        controller.Move(moveDirection * Time.deltaTime);
-    }
+	    transform.Rotate(new Vector3(0, Input.GetAxis("Camera X"), 0) * mouseSensitivity);
+	    cameraTransform.Rotate(new Vector3(Input.GetAxis("Camera Y"), 0, 0) * mouseSensitivity);
+
+
+	    if (controller.isGrounded)
+	    {
+		    moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		    moveDirection = transform.TransformDirection(moveDirection); // Deal with rotation
+		    moveDirection *= speed;
+
+		    if (Input.GetButton("Sprint"))
+		    {
+			    moveDirection *= sprintMultiplier;
+		    }
+
+		    if (Input.GetButton("Jump"))
+		    {
+			    moveDirection.y = jumpSpeed;
+		    }
+	    }
+
+	    moveDirection.y -= gravity * Time.deltaTime; // Gravity
+
+	    controller.Move(moveDirection * Time.deltaTime);
+	    }
 }
