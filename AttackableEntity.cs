775e25ac8e2d9603b09a3cf27f9239868fa97849
Eoin <eoin.t.moran00@gmail.com>
2020-06-07 18:51:58 +0100

    Added game state manager

---
+++ AttackableEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Mirror;
+
+public class AttackableEntity : NetworkBehaviour
+{
+	public float health = 100.0f; // Storing health as an int to allow for fractional health caused by percentage modifiers
+
+	public int GetHealthInt()
+    {
+	    return (int) Math.Ceiling(health); // Using Ceil to avoid a situation where a player is still alive with "0" health
+    }
+
+    public void TakeDamage(float damage)
+    {
+	    health -= damage;
+    }
+}
---
+++ GameStateManager.cs
@@ -0,0 +1,65 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class GameStateManager: NetworkBehaviour
+{
+	private GameState state;
+
+	public GameState State
+	{
+		get
+		{
+			return state;
+		}
+		set
+		{
+			switch (value)
+			{
+				case GameState.PAUSED :
+					Cursor.lockState = CursorLockMode.None;
+					Cursor.visible = false;
+					state = value;
+					break;
+				case GameState.IN_GAME :
+					Cursor.lockState = CursorLockMode.Locked;
+					Cursor.visible = false;
+					state = value;
+					break;
+				default:
+					state = value;
+					break;
+			}
+
+			Debug.Log(state);
+		}
+	}
+
+	void Awake()
+	{
+		State = GameState.DISCONNECTED;
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Escape") && state == GameState.IN_GAME)
+		{
+			State = GameState.PAUSED;
+		} else if (Input.GetButtonDown("Escape") && state == GameState.PAUSED)
+		{
+			State = GameState.IN_GAME;
+		}
+	}
+
+	public override void OnStartLocalPlayer()
+	{
+		State = GameState.IN_GAME;
+		Debug.Log("Connected");
+	}
+
+	public enum GameState
+    {
+	    IN_GAME,
+	    PAUSED,
+	    DISCONNECTED
+    }
+}
